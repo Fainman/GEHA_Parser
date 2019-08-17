d65523b3a094bc46e2faf91ee163561c5f54b7e5
John Guerra <JohnMGuerra@gmail.com>
2019-08-16 22:03:59 -0600

    Create parser for claim headers

--- GEHA Text Parse.py
+++ GEHA Text Parse.py
@@ -2,20 +2,20 @@
 Import GEHA data and output to .csv
 """
 
-import os, sys, csv
+import os, more_itertools, sys, csv
 
 
 def process_file(path, file):
     os.chdir(path)
 
     with open(file, "r", encoding="latin-1") as data:
-        for line in data:
+        for line, next_line in more_itertools.pairwise(data):
 
             if line.startswith("Member ID:"):       # When this text is seen,
-                is_claim_summary = True             # it marks the start of a claim summary
-                claim_summary = []                  # and a new claim_summary is created
+                is_claim_header = True             # it marks the start of a claim summary
+                claim_header = []                  # and a new claim_header is created
 
-            if is_claim_summary:
+            if is_claim_header:
                 if line.startswith("Member ID:") or \
                         line.startswith("Patient DOB:") or \
                         line.startswith("Patient Acct. No.:") or \
@@ -23,27 +23,26 @@ def process_file(path, file):
                         line.startswith("Status:") or \
                         line.startswith("Processed Date:") or \
                         line.startswith("EOB:"):
-                    claim_summary.append(line.split(" ")[-1])
+                    claim_header.append(line.split(" ")[-1].rstrip())
                 elif line.startswith("Patient Name:"):
-                    claim_summary.append(line.split(" ")[-2])
-                elif line.startswith("BALANCE OF COVERED CHARGES APPLIED"):     # When this text is seen,
-                    is_claim_summary = False                                    # it marks the end of a claim summary
-                    continue
-                elif line == "Date of Service" or \
-                        line == "Total Charges" or \
-                        line == "Max Allowed PPO Charges" or \
-                        line == "Not Covered" or \
-                        line == "Note":
-                    continue
-                else:
-                    claim_summary.append(line)
+                    claim_header.append(line.split(" ")[-2].rstrip())
+                if line.startswith("EOB: "):                               # When this text is seen,
+                    is_claim_header = False                                # it marks the end of a claim header
+                continue
 
-            if line.contains(":"):
-                continue
-            if line.startswith("Date of Service") or \
-                line.startswith("Total Charges") or \
-                line.startswith("Date of Service"):
-                continue
+            # if not is_claim_header:
+            #     if not(line.startswith("Allowable") or
+            #            line.startswith("Deductible") or
+            #            line.startswith("Copay / Coinsurance") or
+            #            line.startswith("Benefit") or
+            #            line.startswith("Summary")):
+            #         continue
+            #     elif not(line.startswith("Date of Service") or
+            #              line.startswith("Total Charges") or
+            #              line.startswith("Max Allowed PPO Charges") or
+            #              line.startswith("Not Covered") or
+            #              line.startswith("Note")):
+            #         claim_header.append(line.rstrip())
 
 
 def main():
@@ -51,7 +50,7 @@ def main():
     Test function for words library
     :return: nothing
     """
-    process_file(r"C:\Users\JohnM\Desktop", "GEHA Data")
+    process_file(r"C:\Users\JohnM\Desktop", "GEHA Data.txt")
 
 
 if __name__ == "__main__":
