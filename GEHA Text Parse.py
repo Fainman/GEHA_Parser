c98b9fa43a467fc9a99ed83ebacd3026d19d8114
John Guerra <JohnMGuerra@gmail.com>
2019-08-23 11:29:26 -0600

    Update to create .CSV Still needs some cleanup and refactoring

--- GEHA Text Parse.py
+++ GEHA Text Parse.py
@@ -2,7 +2,7 @@
 Import GEHA data and output to .csv
 """
 
-import os, more_itertools, sys, csv
+import os, more_itertools, sys, csv, pprint
 
 
 def process_file(path, file):
@@ -13,11 +13,14 @@ def process_file(path, file):
     claim_detail_output = []
     claim_header = []
     claim_header_output = []
+    claim_summary = []
+    claim_summary_output = []
 
     with open(file, "r", encoding="latin-1") as data:
         for line, next_line in more_itertools.pairwise(data):
 
-            section = determine_section(line, section)
+            if determine_section(line) is not None:
+                section = determine_section(line)
 
             if section == "header":
                 if line.startswith("Claim Number:"):
@@ -29,11 +32,10 @@ def process_file(path, file):
                     claim_header.append(line.split(" ")[-1].rstrip())
                 if line.startswith("Processed Date:"):              # When this text is seen,
                     claim_header_output.append(claim_header)        # it marks the end
-                    claim_header.clear()                            # of the header section
+                    claim_header = []                               # of the header section
                     section = "claim_detail"                        # and the start of the claim detail
-                    #print(claim_header_output)
             elif section == "claim_detail":
-                if not claim_detail:
+                if not claim_detail and next_line != "Summary\n":
                     claim_detail.append(next_line.rstrip())
                 if line.startswith("Date of Service") or \
                         line.startswith("Total Charges") or \
@@ -46,20 +48,53 @@ def process_file(path, file):
                         line.startswith("Benefit"):
                     claim_detail.append(next_line.rstrip())
                 if line.startswith("Benefit"):
-                    print([len(claim_detail)] + [claim_number] + claim_detail)
-                    claim_detail_output.append(claim_detail)
-                    claim_detail.clear()
+                    #print([len(claim_detail)] + [claim_number] + claim_detail)
+                    claim_detail_output.append([claim_number] + claim_detail)
+                    claim_detail = []
             elif section == "summary":
-                pass
+                if line.startswith("PAYEE	CHECK #	AMOUNT"):                   # If end of summary, do clean up
+                    if next_line.startswith("Member ID: "):                     # If next line is member id,
+                        claim_summary += ["", "", ""]                           # there's no info to obtain.
+                    else:                                                       # Otherwise,
+                        next_line = next_line.rstrip()                          # remove the \n
+                        claim_summary += next_line.split("\t")                  # and obtain the data
+                    claim_summary_output.append([claim_number] + claim_summary)     # Create output
+                    claim_summary = []                                              # Reset it to [] for next time
+                elif line.startswith("Benefits Payable") or \
+                        line.startswith("Paid by Other Plan") or \
+                        line.startswith("Other Plan Paid Adjustment") or \
+                        line.startswith("Other Adjustment") or \
+                        line.startswith("Other Adjustment Reason") or \
+                        line.startswith("Total Paid by GEHA") or \
+                        line.startswith("Patient Owes Provider"):
+                    claim_summary.append(line.split("\t")[-1].rstrip())
+
+    # Output to CSV
+    with open('headers.csv', mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['Patient DOB', 'Patient Acct. No.', 'Claim Number', 'Status', 'Processed Date'])
+        for row in claim_header_output:
+            csv_writer.writerow(row)
+    with open('detail.csv', mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['Provider Name', 'Date of Service', 'Total Charges', 'Max Allowed PPO Charges', 'Not Covered',
+                            'Note', 'Allowable', 'Deductible', 'Copay / Coinsurance', 'Benefit'])
+        for row in claim_detail_output:
+            csv_writer.writerow(row)
+    with open('summary.csv', 'w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['Patient DOB', 'Patient Acct. No.', 'Claim Number', 'Status', 'Processed Date'])
+        for row in claim_summary_output:
+            csv_writer.writerow(row)
 
 
-def determine_section(line, section):
-    if ":" in line and "EOB: View" not in line:   # When this text is seen,
-        return "header"                 # it marks the start of a claim summary
+def determine_section(line):
+    if ":" in line and "EOB: View" not in line:     # When this text is seen,
+        return "header"                             # it marks the start of a claim summary
     elif line == "Summary\n":
         return "summary"
     else:
-        return section
+        return None                                 # No change
 
 
 def main():
